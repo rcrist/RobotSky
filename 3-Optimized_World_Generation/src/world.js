fcf5a0609b82b782b4fac2ea3f519fd0b6c3e2c2
rcrist <rickcrist99@gmail.com>
2025-03-01 08:12:32 -0700

    Optimized world generation

---
+++ 3-Optimized_World_Generation/src/world.js
@@ -0,0 +1,31 @@
+import * as THREE from 'three';
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } );
+
+export class World extends THREE.Group {
+    constructor(size = {width: 64, height: 32}) {
+        super();
+        this.size = size;
+    }
+
+    generate() {
+        this.clear();
+        
+        const maxCount = this.size.width * this.size.width * this.size.height;
+        const mesh = new THREE.InstancedMesh(geometry, material, maxCount);
+        mesh.count = 0;
+
+        const matrix = new THREE.Matrix4(); // Stores the position of the block
+        for (let x = 0; x < this.size.width; x++) {
+            for (let y = 0; y < this.size.height; y++) {
+                for (let z = 0; z < this.size.width; z++) {
+                    matrix.setPosition(x + 0.5, y + 0.5, z + 0.5);
+                    mesh.setMatrixAt(mesh.count++, matrix);
+                }
+            }
+        }
+
+        this.add(mesh);
+    }
+}
