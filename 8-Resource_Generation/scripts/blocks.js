47984fef88f8b98537b2d6a7e2db08a320b85e4d
rcrist <rickcrist99@gmail.com>
2025-03-01 08:14:45 -0700

    Resource generation

---
+++ 8-Resource_Generation/scripts/blocks.js
@@ -0,0 +1,43 @@
+export const blocks = {
+    empty: {
+        id: 0,
+        name: 'empty'
+    },
+    grass: {
+        id: 1,
+        name: 'grass',
+        color: 0x559020
+    },
+    dirt: {
+        id: 2,
+        name: 'dirt',
+        color: 0x807020
+    },
+    stone: {
+        id: 3,
+        name: 'stone',
+        color: 0x808080,
+        scale: { x: 30, y: 30, z: 30 },
+        scarcity: 0.5
+    },
+    coalOre: {
+        id: 4,
+        name: 'coalOre',
+        color: 0x202020,
+        scale: { x: 20, y: 20, z: 20 },
+        scarcity: 0.8
+    },
+    ironOre: {
+        id: 5,
+        name: 'ironOre',
+        color: 0x806060,
+        scale: { x: 60, y: 60, z: 60 },
+        scarcity: 0.9
+    }
+}
+
+export const resources = [
+    blocks.stone,
+    blocks.coalOre,
+    blocks.ironOre
+  ];
---
+++ 8-Resource_Generation/scripts/ui.js
@@ -0,0 +1,30 @@
+import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { resources } from './blocks.js';
+
+export function createUI(world) {
+    const gui = new GUI();
+
+    const worldFolder = gui.addFolder('World');
+    worldFolder.add(world.size, 'width', 8, 128, 1).name('Width');
+    worldFolder.add(world.size, 'height', 8, 64, 1).name('Height');
+
+    const terrainFolder = gui.addFolder('Terrain');
+    terrainFolder.add(world.params, 'seed', 1, 10000).name('Seed');
+    terrainFolder.add(world.params.terrain, 'scale', 10, 100).name('Scale');
+    terrainFolder.add(world.params.terrain, 'magnitude', 0, 1).name('Magnitude');
+    terrainFolder.add(world.params.terrain, 'offset', 0, 1).name('Offset');
+
+    resources.forEach(resource => {
+        const resourcesFolder = gui.addFolder(resource.name);
+        resourcesFolder.add(resource, 'scarcity', 0, 1).name('Scarcity');
+
+        const scaleFolder = resourcesFolder.addFolder('Scale');
+        scaleFolder.add(resource.scale, 'x', 10, 100).name('X Scale');
+        scaleFolder.add(resource.scale, 'y', 10, 100).name('Y Scale');
+        scaleFolder.add(resource.scale, 'z', 10, 100).name('Z Scale');
+    });
+
+    gui.onChange(() => {
+        world.generate();
+    });
+}
